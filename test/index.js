2edcf4c83197efb8a2ab62d64978f499eaacf31c
Maximiline Thayer <garbados@gmail.com>
2014-06-13 18:37:15 -0700

    tests pass, let's see if travis agress

--- test/index.js
+++ test/index.js
@@ -2,43 +2,68 @@ var record = require('./record');
 var nano = require('nano');
 var orchestrate = require('orchestrate');
 var OrchestrateCouchDB = require('../lib-cov');
+var async = require('async');
 
 describe('OrchestrateCouchDB', function () {
   var recorder = record('OrchestrateCouchDB');
   before(recorder.before);
 
-  before(function (done) {
-    // start watcher
-    this.watcher = OrchestrateCouchDB();
+  before(function () {
     // set up db connections
     this.collection = 'orchestrate-couchdb';
     this.nano = nano('http://localhost:5984');
-    this.couchdb = this.nano.use(this.database);
+    this.couchdb = this.nano.use(this.collection);
     this.orc = orchestrate(process.env.ORCHESTRATE_API_KEY);
   });
 
-  beforeEach(function (done) {
+  before(function (done) {
     // create couchdb database
-    this.nano.db.create(this.collection, done);
+    this.nano.db.create(this.collection, function (err) {
+      done(err);
+    });
   });
 
-  afterEach(function (done) {
+  before(function () {
+    // start watcher
+    this.watcher = OrchestrateCouchDB({
+      couchdb: {
+        database: this.collection
+      }
+    });
+  });
+
+  after(function (done) {
+    var self = this;
     // delete collections
     async.parallel([
-      this.nano.db.destroy.bind(this.nano.db, this.collection),
-      this.orc.deleteCollection.bind(this.orc, this.collection)
+      function (done) {
+        self.nano.db.destroy(self.collection, done);
+      },
+      function (done) {
+        self.orc.deleteCollection(self.collection)
+        .then(function () {
+          done();
+        })
+        .fail(done);
+      }
     ], done);
   });
 
   it('should sync CouchDB changes to Orchestrate', function (done) {
+    var self = this;
     var id = 'derpderp';
     // write documents to couchdb
-    this.couchdb.insert(id, {}, function (err) {
+    this.couchdb.insert({
+      hello: 'goodbye'
+    }, id, function (err) {
       if (err) done(err);
     });
     // wait for the watcher to report the change
-    this.watcher.on('change.success', function (change_id) {
-      if (id === change_id) done();
+    this.watcher.on('change.success', function (change) {
+      if (id === change.id) {
+        self.watcher.stream.stop();
+        done();
+      }
     });
   });
 
